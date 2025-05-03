4a3a6e15a41c460be24f49d6712b0c6e7cfe3fcc
Your Name <you@example.com>
2025-05-03 12:41:01 +0100

    akhir ata7aryat

--- lem-in/main.go
+++ lem-in/main.go
@@ -22,7 +22,7 @@ func main() {
 		fmt.Println("No valid paths found.")
 		os.Exit(1)
 	}
-
+fmt.Println(paths,"\n")
 	
 	fmt.Println(string(parsing.File) + "\n")
 	utils.Filter = graph.FindDisjointPaths(paths, colony)
